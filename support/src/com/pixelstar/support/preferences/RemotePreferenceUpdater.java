w1zzyl/vendor_awaken

support: Import Preferences from lineage
-----

support: Stop using PreferenceScreen styles for preferences

* until it's fixed in androidx

Change-Id: Ia9c1b0edb2066ac91172ab202657b01252c3972d

Co-authored-by: Volodymyr Zhdanov <wight554@gmail.com>
Change-Id: Ie870662114645afa4f5193754d98d5b7a1cd0b46

support/src/com/awaken/support/preferences/RemotePreferenceUpdater.java
@@ -1,0 +1,127 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.awaken.support.preferences;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.util.Log;
+
+import java.util.Objects;
+
+/**
+ * Base class for remote summary providers.
+ * <p>
+ * When an application is hosting preferences which are served by a different process,
+ * the former needs to stay updated with changes in order to display the correct
+ * summary when the user returns to the latter.
+ * <p>
+ * This class implements a simple ordered broadcast mechanism where the application
+ * running the RemotePreference sends an explicit broadcast to the host, who
+ * fills out the extras in the result bundle and returns it to the caller.
+ * <p>
+ * A minimal implementation will override getSummary and return a summary
+ * for the given key. Alternatively, fillResultExtras can be overridden
+ * if additional data should be added to the result.
+ */
+public class RemotePreferenceUpdater extends BroadcastReceiver {
+
+    private static final String TAG = RemotePreferenceUpdater.class.getSimpleName();
+
+    private static final boolean DEBUG = Log.isLoggable(
+            RemotePreference.class.getSimpleName(), Log.VERBOSE);
+
+    private static Intent getTargetIntent(Context context, String key) {
+        final Intent i = new Intent(RemotePreference.ACTION_REFRESH_PREFERENCE);
+        i.putExtra(RemotePreference.EXTRA_KEY, key);
+        return i;
+    }
+
+    /**
+     * Fetch the updated summary for the given key
+     *
+     * @param key
+     * @return the summary for the given key
+     */
+    protected String getSummary(Context context, String key) {
+        return null;
+    }
+
+
+    /**
+     * Fill the bundle with the summary and any other data needed  to update
+     * the client.
+     *
+     * @param context
+     * @param key
+     * @param extras
+     * @return true if successful
+     */
+    protected boolean fillResultExtras(Context context, String key, Bundle extras) {
+        extras.putString(RemotePreference.EXTRA_KEY, key);
+
+        final String summary = getSummary(context, key);
+        if (summary == null) {
+            return false;
+        }
+
+        extras.putString(RemotePreference.EXTRA_SUMMARY, summary);
+        return true;
+    }
+
+
+    /**
+     * @hide
+     */
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (isOrderedBroadcast() &&
+                RemotePreference.ACTION_UPDATE_PREFERENCE.equals(intent.getAction())) {
+            final String key = intent.getStringExtra(RemotePreference.EXTRA_KEY);
+            if (DEBUG) Log.d(TAG, "onReceive key=" +key +
+                       " intent=" + Objects.toString(intent) +
+                       " extras=" + Objects.toString(intent.getExtras()));
+
+            if (key != null) {
+                if (fillResultExtras(context, key, getResultExtras(true))) {
+                    setResultCode(Activity.RESULT_OK);
+
+                    if (DEBUG) Log.d(TAG, "onReceive result=" +
+                               Objects.toString(getResultExtras(true)));
+                    return;
+                }
+            }
+            abortBroadcast();
+        }
+    }
+
+    /**
+     * Tell the RemotePreference that updated state is available. Call from
+     * the fragment when necessary.
+     *
+     * @param context
+     * @param key
+     */
+    public static void notifyChanged(Context context, String key) {
+        if (DEBUG) Log.d(TAG, "notifyChanged: key=" + key +
+                   " target=" + Objects.toString(getTargetIntent(context, key)));
+        context.sendBroadcastAsUser(getTargetIntent(context, key),
+                UserHandle.CURRENT, "lineageos.permission.MANAGE_REMOTE_PREFERENCES");
+    }
+}

support/src/com/awaken/support/preferences/SelfRemovingPreference.java
@@ -1,0 +1,73 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.awaken.support.preferences;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceViewHolder;
+
+import com.awaken.support.preferences.ConstraintsHelper;
+
+/**
+ * A Preference which can automatically remove itself from the hierarchy
+ * based on constraints set in XML.
+ */
+public class SelfRemovingPreference extends Preference {
+
+    private final ConstraintsHelper mConstraints;
+
+    public SelfRemovingPreference(Context context, AttributeSet attrs,
+                                  int defStyle, int defStyleRes) {
+        super(context, attrs, defStyle, defStyleRes);
+        mConstraints = new ConstraintsHelper(context, attrs, this);
+    }
+
+    public SelfRemovingPreference(Context context, AttributeSet attrs, int defStyle) {
+        this(context, attrs, defStyle, 0);
+    }
+
+    public SelfRemovingPreference(Context context, AttributeSet attrs) {
+        this(context, attrs, ConstraintsHelper.getAttr(context,
+                androidx.preference.R.attr.preferenceStyle,
+                android.R.attr.preferenceStyle));
+    }
+
+    public SelfRemovingPreference(Context context) {
+        this(context, null);
+    }
+
+    @Override
+    public void onAttached() {
+        super.onAttached();
+        mConstraints.onAttached();
+    }
+
+    @Override
+    public void onBindViewHolder(PreferenceViewHolder holder) {
+        super.onBindViewHolder(holder);
+        mConstraints.onBindViewHolder(holder);
+    }
+
+    public void setAvailable(boolean available) {
+        mConstraints.setAvailable(available);
+    }
+
+    public boolean isAvailable() {
+        return mConstraints.isAvailable();
+    }
+}

support/src/com/awaken/support/preferences/SelfRemovingSwitchPreference.java
@@ -1,0 +1,106 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ * Copyright (C) 2018 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.awaken.support.preferences;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.preference.PreferenceDataStore;
+import androidx.preference.PreferenceViewHolder;
+import androidx.preference.SwitchPreference;
+
+/**
+ * A SwitchPreference which can automatically remove itself from the hierarchy
+ * based on constraints set in XML.
+ */
+public abstract class SelfRemovingSwitchPreference extends SwitchPreference {
+
+    private final ConstraintsHelper mConstraints;
+
+    public SelfRemovingSwitchPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        mConstraints = new ConstraintsHelper(context, attrs, this);
+        setPreferenceDataStore(new DataStore());
+    }
+
+    public SelfRemovingSwitchPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mConstraints = new ConstraintsHelper(context, attrs, this);
+        setPreferenceDataStore(new DataStore());
+    }
+
+    public SelfRemovingSwitchPreference(Context context) {
+        super(context);
+        mConstraints = new ConstraintsHelper(context, null, this);
+        setPreferenceDataStore(new DataStore());
+    }
+
+    @Override
+    public void onAttached() {
+        super.onAttached();
+        mConstraints.onAttached();
+    }
+
+    @Override
+    public void onBindViewHolder(PreferenceViewHolder holder) {
+        super.onBindViewHolder(holder);
+        mConstraints.onBindViewHolder(holder);
+    }
+
+    public void setAvailable(boolean available) {
+        mConstraints.setAvailable(available);
+    }
+
+    public boolean isAvailable() {
+        return mConstraints.isAvailable();
+    }
+
+    protected abstract boolean isPersisted();
+    protected abstract void putBoolean(String key, boolean value);
+    protected abstract boolean getBoolean(String key, boolean defaultValue);
+
+    @Override
+    protected void onSetInitialValue(boolean restorePersistedValue, Object defaultValue) {
+        final boolean checked;
+        if (!restorePersistedValue || !isPersisted()) {
+            if (defaultValue == null) {
+                return;
+            }
+            checked = (boolean) defaultValue;
+            if (shouldPersist()) {
+                persistBoolean(checked);
+            }
+        } else {
+            // Note: the default is not used because to have got here
+            // isPersisted() must be true.
+            checked = getBoolean(getKey(), false /* not used */);
+        }
+        setChecked(checked);
+    }
+
+    private class DataStore extends PreferenceDataStore {
+        @Override
+        public void putBoolean(String key, boolean value) {
+            SelfRemovingSwitchPreference.this.putBoolean(key, value);
+        }
+
+        @Override
+        public boolean getBoolean(String key, boolean defaultValue) {
+            return SelfRemovingSwitchPreference.this.getBoolean(key, defaultValue);
+        }
+    }
+}

suvojit213/vendor_pixelstar

support/src/com/pixelstar/support/preferences/RemotePreferenceUpdater.java
@@ -1,0 +1,127 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.pixelstar.support.preferences;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.util.Log;
+
+import java.util.Objects;
+
+/**
+ * Base class for remote summary providers.
+ * <p>
+ * When an application is hosting preferences which are served by a different process,
+ * the former needs to stay updated with changes in order to display the correct
+ * summary when the user returns to the latter.
+ * <p>
+ * This class implements a simple ordered broadcast mechanism where the application
+ * running the RemotePreference sends an explicit broadcast to the host, who
+ * fills out the extras in the result bundle and returns it to the caller.
+ * <p>
+ * A minimal implementation will override getSummary and return a summary
+ * for the given key. Alternatively, fillResultExtras can be overridden
+ * if additional data should be added to the result.
+ */
+public class RemotePreferenceUpdater extends BroadcastReceiver {
+
+    private static final String TAG = RemotePreferenceUpdater.class.getSimpleName();
+
+    private static final boolean DEBUG = Log.isLoggable(
+            RemotePreference.class.getSimpleName(), Log.VERBOSE);
+
+    private static Intent getTargetIntent(Context context, String key) {
+        final Intent i = new Intent(RemotePreference.ACTION_REFRESH_PREFERENCE);
+        i.putExtra(RemotePreference.EXTRA_KEY, key);
+        return i;
+    }
+
+    /**
+     * Fetch the updated summary for the given key
+     *
+     * @param key
+     * @return the summary for the given key
+     */
+    protected String getSummary(Context context, String key) {
+        return null;
+    }
+
+
+    /**
+     * Fill the bundle with the summary and any other data needed  to update
+     * the client.
+     *
+     * @param context
+     * @param key
+     * @param extras
+     * @return true if successful
+     */
+    protected boolean fillResultExtras(Context context, String key, Bundle extras) {
+        extras.putString(RemotePreference.EXTRA_KEY, key);
+
+        final String summary = getSummary(context, key);
+        if (summary == null) {
+            return false;
+        }
+
+        extras.putString(RemotePreference.EXTRA_SUMMARY, summary);
+        return true;
+    }
+
+
+    /**
+     * @hide
+     */
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (isOrderedBroadcast() &&
+                RemotePreference.ACTION_UPDATE_PREFERENCE.equals(intent.getAction())) {
+            final String key = intent.getStringExtra(RemotePreference.EXTRA_KEY);
+            if (DEBUG) Log.d(TAG, "onReceive key=" +key +
+                       " intent=" + Objects.toString(intent) +
+                       " extras=" + Objects.toString(intent.getExtras()));
+
+            if (key != null) {
+                if (fillResultExtras(context, key, getResultExtras(true))) {
+                    setResultCode(Activity.RESULT_OK);
+
+                    if (DEBUG) Log.d(TAG, "onReceive result=" +
+                               Objects.toString(getResultExtras(true)));
+                    return;
+                }
+            }
+            abortBroadcast();
+        }
+    }
+
+    /**
+     * Tell the RemotePreference that updated state is available. Call from
+     * the fragment when necessary.
+     *
+     * @param context
+     * @param key
+     */
+    public static void notifyChanged(Context context, String key) {
+        if (DEBUG) Log.d(TAG, "notifyChanged: key=" + key +
+                   " target=" + Objects.toString(getTargetIntent(context, key)));
+        context.sendBroadcastAsUser(getTargetIntent(context, key),
+                UserHandle.CURRENT, "lineageos.permission.MANAGE_REMOTE_PREFERENCES");
+    }
+}

support/src/com/pixelstar/support/preferences/SelfRemovingPreference.java
@@ -1,0 +1,73 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.pixelstar.support.preferences;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceViewHolder;
+
+import com.pixelstar.support.preferences.ConstraintsHelper;
+
+/**
+ * A Preference which can automatically remove itself from the hierarchy
+ * based on constraints set in XML.
+ */
+public class SelfRemovingPreference extends Preference {
+
+    private final ConstraintsHelper mConstraints;
+
+    public SelfRemovingPreference(Context context, AttributeSet attrs,
+                                  int defStyle, int defStyleRes) {
+        super(context, attrs, defStyle, defStyleRes);
+        mConstraints = new ConstraintsHelper(context, attrs, this);
+    }
+
+    public SelfRemovingPreference(Context context, AttributeSet attrs, int defStyle) {
+        this(context, attrs, defStyle, 0);
+    }
+
+    public SelfRemovingPreference(Context context, AttributeSet attrs) {
+        this(context, attrs, ConstraintsHelper.getAttr(context,
+                androidx.preference.R.attr.preferenceStyle,
+                android.R.attr.preferenceStyle));
+    }
+
+    public SelfRemovingPreference(Context context) {
+        this(context, null);
+    }
+
+    @Override
+    public void onAttached() {
+        super.onAttached();
+        mConstraints.onAttached();
+    }
+
+    @Override
+    public void onBindViewHolder(PreferenceViewHolder holder) {
+        super.onBindViewHolder(holder);
+        mConstraints.onBindViewHolder(holder);
+    }
+
+    public void setAvailable(boolean available) {
+        mConstraints.setAvailable(available);
+    }
+
+    public boolean isAvailable() {
+        return mConstraints.isAvailable();
+    }
+}

support/src/com/pixelstar/support/preferences/SelfRemovingSwitchPreference.java
@@ -1,0 +1,106 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ * Copyright (C) 2018 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.pixelstar.support.preferences;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.preference.PreferenceDataStore;
+import androidx.preference.PreferenceViewHolder;
+import androidx.preference.SwitchPreference;
+
+/**
+ * A SwitchPreference which can automatically remove itself from the hierarchy
+ * based on constraints set in XML.
+ */
+public abstract class SelfRemovingSwitchPreference extends SwitchPreference {
+
+    private final ConstraintsHelper mConstraints;
+
+    public SelfRemovingSwitchPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        mConstraints = new ConstraintsHelper(context, attrs, this);
+        setPreferenceDataStore(new DataStore());
+    }
+
+    public SelfRemovingSwitchPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mConstraints = new ConstraintsHelper(context, attrs, this);
+        setPreferenceDataStore(new DataStore());
+    }
+
+    public SelfRemovingSwitchPreference(Context context) {
+        super(context);
+        mConstraints = new ConstraintsHelper(context, null, this);
+        setPreferenceDataStore(new DataStore());
+    }
+
+    @Override
+    public void onAttached() {
+        super.onAttached();
+        mConstraints.onAttached();
+    }
+
+    @Override
+    public void onBindViewHolder(PreferenceViewHolder holder) {
+        super.onBindViewHolder(holder);
+        mConstraints.onBindViewHolder(holder);
+    }
+
+    public void setAvailable(boolean available) {
+        mConstraints.setAvailable(available);
+    }
+
+    public boolean isAvailable() {
+        return mConstraints.isAvailable();
+    }
+
+    protected abstract boolean isPersisted();
+    protected abstract void putBoolean(String key, boolean value);
+    protected abstract boolean getBoolean(String key, boolean defaultValue);
+
+    @Override
+    protected void onSetInitialValue(boolean restorePersistedValue, Object defaultValue) {
+        final boolean checked;
+        if (!restorePersistedValue || !isPersisted()) {
+            if (defaultValue == null) {
+                return;
+            }
+            checked = (boolean) defaultValue;
+            if (shouldPersist()) {
+                persistBoolean(checked);
+            }
+        } else {
+            // Note: the default is not used because to have got here
+            // isPersisted() must be true.
+            checked = getBoolean(getKey(), false /* not used */);
+        }
+        setChecked(checked);
+    }
+
+    private class DataStore extends PreferenceDataStore {
+        @Override
+        public void putBoolean(String key, boolean value) {
+            SelfRemovingSwitchPreference.this.putBoolean(key, value);
+        }
+
+        @Override
+        public boolean getBoolean(String key, boolean defaultValue) {
+            return SelfRemovingSwitchPreference.this.getBoolean(key, defaultValue);
+        }
+    }
+}