Project-Awaken/android_vendor_awaken

overlay: Move out CertifiedPropsOverlay
* Also include our certification repo while we're at it.

Change-Id: I55c23ee67b11ff79e64a3407f2a28ebe795ee295

config/common.mk
@@ -251,16 +251,13 @@ PRODUCT_PACKAGE_OVERLAYS += \
     vendor/awaken/overlay/no-rro
 
 PRODUCT_PACKAGES += \
-    CertifiedPropsOverlay \
     CustomPixelLauncherOverlay \
     DocumentsUIOverlay \
     ImmersiveNavigationOverlay \
     NetworkStackOverlay \
     NexusLauncherResOverlay
 
-PRODUCT_COPY_FILES += \
-    vendor/awaken/config/config-system_ext.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/overlay/config/config.xml
-
+include vendor/awaken/certification/config.mk
 include vendor/awaken/config/branding.mk
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk

suvojit213/vendor_pixelstar

config/common.mk
@@ -259,7 +259,6 @@ PRODUCT_PACKAGE_OVERLAYS += \
     vendor/pixelstar/overlay/no-rro
 
 PRODUCT_PACKAGES += \
-    CertifiedPropsOverlay \
     CustomPixelLauncherOverlay \
     DocumentsUIOverlay \
     ImmersiveNavigationOverlay \
@@ -271,8 +270,7 @@ $(call inherit-product, vendor/pixelstar/config/bootanimation.mk)
 
 include vendor/pixelstar/config/branding.mk
 
-PRODUCT_COPY_FILES += \
-    vendor/pixelstar/config/config-system_ext.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/overlay/config/config.xml
+include vendor/pixelstar/certification/config.mk
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 -include vendor/pixelstar/config/partner_gms.mk