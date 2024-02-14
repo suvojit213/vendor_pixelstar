bananadroid/android_vendor_banana

soong: Add TARGET_INCLUDES_MIUI_CAMERA
- Inspired from 11f751a76380eca5e5bd1b5db329ff9fc4c732df

config/BoardConfigSoong.mk
@@ -45,6 +45,7 @@ SOONG_CONFIG_bananaGlobalVars += \
     uses_oplus_camera \
     uses_oppo_camera \
     uses_nothing_camera \
+    include_miui_camera \
     has_legacy_camera_hal1 \
     has_memfd_backport \
     target_health_charging_control_charging_path \
@@ -92,6 +93,7 @@ SOONG_CONFIG_bananaGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
 SOONG_CONFIG_bananaGlobalVars_uses_oppo_camera := $(TARGET_USES_OPPO_CAMERA)
 SOONG_CONFIG_bananaGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)
 SOONG_CONFIG_bananaGlobalVars_uses_miui_camera := $(TARGET_USES_MIUI_CAMERA)
+SOONG_CONFIG_bananaGlobalVars_include_miui_camera := $(TARGET_INCLUDES_MIUI_CAMERA)
 SOONG_CONFIG_bananaGlobalVars_has_legacy_camera_hal1 := $(TARGET_HAS_LEGACY_CAMERA_HAL1)
 SOONG_CONFIG_bananaGlobalVars_has_memfd_backport := $(TARGET_HAS_MEMFD_BACKPORT)
 SOONG_CONFIG_bananaGlobalVars_uses_legacy_fd_fbdev := $(TARGET_USES_LEGACY_FD_FBDEV)

suvojit213/vendor_pixelstar

Change-Id: I72b74aa5dfcd25cda70f8b754c58fe1af38163db
Signed-off-by: S I P U N <sipunkumar85@gmail.com>

config/BoardConfigSoong.mk
@@ -36,6 +36,7 @@ SOONG_CONFIG_pixelstarGlobalVars += \
     disable_bluetooth_le_set_host_feature \
     gralloc_handle_has_custom_content_md_reserved_size \
     gralloc_handle_has_reserved_size \
+    include_miui_camera \
     target_health_charging_control_charging_path \
     target_health_charging_control_charging_enabled \
     target_health_charging_control_charging_disabled \
@@ -82,6 +83,7 @@ SOONG_CONFIG_pixelstarNvidiaVars_uses_nvidia_enhancements := $(NV_ANDROID_FRAMEW
 SOONG_CONFIG_pixelstarQcomVars_qti_vibrator_use_effect_stream := $(TARGET_QTI_VIBRATOR_USE_EFFECT_STREAM)
 SOONG_CONFIG_pixelstarQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
 SOONG_CONFIG_pixelstarQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
+SOONG_CONFIG_pixelstarGlobalVars_include_miui_camera := $(TARGET_INCLUDES_MIUI_CAMERA)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0